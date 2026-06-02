73b46f689d62f977cb2a26e9afc7224cba1a7f40
Nuuvsen <nuuvsen@gmail.com>
2026-06-01 21:13:54 -0300

    Atualizando projeto

--- firebase.js
+++ firebase.js
@@ -1,4 +1,19 @@
 const admin = require('firebase-admin');
+
+let serviceAccount;
+
+// Verifica se está no servidor (Portainer) ou no seu computador local
+if (process.env.FIREBASE_JSON) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_JSON);
+} else {
+  serviceAccount = require('./firebase-key.json');
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// ... resto do seu código (exportar db, etc)
 const serviceAccount = require('./firebase-key.json');
 
 // Inicializa a conexão com o banco usando a sua chave secreta
